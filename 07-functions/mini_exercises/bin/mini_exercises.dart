e3990d750c4a23cf5d309954390ab60c15af10c7
kipkoechke <chepkwonyke1@gmail.com>
2022-12-17 11:56:54 +0300

    Did exercise 1

---
+++ 07-functions/mini_exercises/bin/mini_exercises.dart
@@ -0,0 +1,12 @@
+void main() {
+  print(youAreWonderful('Bob'));
+}
+
+// Mini-exercises
+// 1. Write a function named youAreWonderful, with a String
+// parameter called name. It should return a string using
+// name, and say something like “You’re wonderful, Bob.”
+
+String youAreWonderful(String name) {
+  return 'You\'re wonderful, $name';
+}
