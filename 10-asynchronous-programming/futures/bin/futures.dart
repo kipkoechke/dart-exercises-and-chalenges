fec5ceee5862fdf9d67d93d7f2514ca7612f83c4
kipkoechke <chepkwonyke1@gmail.com>
2023-02-01 21:50:22 +0300

    Made a get request

--- 10-asynchronous-programming/futures/bin/futures.dart
+++ 10-asynchronous-programming/futures/bin/futures.dart
@@ -1,21 +1,21 @@
-Future<void> main() async {
-  print('Before the future');
-  try {
-  final value = await Future<int>.delayed(
-      Duration(seconds: 1),
-      () => 42,
-    );
-    throw Exception('There was an error');
-    print('Value: $value');
-   
-  } catch (error) {
-    print(error);
-  } finally {
-    print('Future is compleete');
-  }
-  print('After the future');
-}
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
+Future<void> main() async {
+final url = 'https://jsonplaceholder.typicode.com/todos/1';
+  final parsedUrl = Uri.parse(url);
+  final response = await http.get(parsedUrl);
+  final statusCode = response.statusCode;
+  if (statusCode == 200) {
+    final rawJsonString = response.body;
+    final jsonMap = jsonDecode(rawJsonString);
+    final todo = Todo.fromJson(jsonMap);
+    print(todo);
+  } else {
+    throw HttpException('$statusCode');
+  }
+}
 class Todo {
   final int userId;
   final int id;
