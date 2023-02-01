c9b62b9a7a0ac6b069146615a30b52152bb6ebfa
kipkoechke <chepkwonyke1@gmail.com>
2023-02-01 17:05:11 +0300

    WOrked on getting results using then callback

--- 10-asynchronous-programming/futures/bin/futures.dart
+++ 10-asynchronous-programming/futures/bin/futures.dart
@@ -2,6 +2,8 @@ void main(List<String> arguments) {
   final myFuture = Future<int>.delayed(
     Duration(seconds: 1),
     () => 42,
+  ).then(
+    (value) => print('Value: $value'),
   );
 
   print(myFuture);
