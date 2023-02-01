264e5c607eb02c89da9f914cf500daf44addd678
kipkoechke <chepkwonyke1@gmail.com>
2023-02-01 17:16:54 +0300

    Worked with try-catch blocks with async-await

--- 10-asynchronous-programming/futures/bin/futures.dart
+++ 10-asynchronous-programming/futures/bin/futures.dart
@@ -1,9 +1,16 @@
 Future<void> main() async {
   print('Before the future');
+  try {
   final value = await Future<int>.delayed(
-    Duration(seconds: 1),
-    () => 42,
-);
-print('Value: $value');
+      Duration(seconds: 1),
+      () => 42,
+    );
+    print('Value: $value');
+   
+  } catch (error) {
+    print(error);
+  } finally {
+    print('Future is compleete');
+  }
   print('After the future');
 }
