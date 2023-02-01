9c505931c049243fcd33450db6094d3353d6f8e0
kipkoechke <chepkwonyke1@gmail.com>
2023-02-01 17:12:18 +0300

    Worked on getting the result with async-await

--- 10-asynchronous-programming/futures/bin/futures.dart
+++ 10-asynchronous-programming/futures/bin/futures.dart
@@ -1,13 +1,9 @@
-void main(List<String> arguments) {
-  final myFuture = Future<int>.delayed(
+Future<void> main() async {
+  print('Before the future');
+  final value = await Future<int>.delayed(
     Duration(seconds: 1),
     () => 42,
-  ).then(
-    (value) => print('Value: $value'),
-      )
-      .catchError(
-        (error) => print('Error:$error'),
-      );
-
-  print(myFuture);
+);
+print('Value: $value');
+  print('After the future');
 }
